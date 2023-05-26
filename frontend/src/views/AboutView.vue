3ab1edf692feb578ae895d6676515aeb9d2ac006
mirsella <mirsella@protonmail.com>
2023-05-26 22:10:55 +0200

    add tailwindss

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -9,20 +9,25 @@ const count = ref(0);
 
 <template>
   <div class="bout">
-    <h1>This is an about page</h1>
+    <h1 class="my-2">This is an about page</h1>
 
 	<!-- rajouter par lucas pour montrer le store pinia -->
-	<h1>couter store:: {{counterStore.count}}</h1>
+	<h1>couter store: {{counterStore.count}}</h1>
 	<h1>couter store: * 2: {{counterStore.doubleCount}}</h1>
-	<button @click="counterStore.increment">increment counter store</button>
+	<button class="button" @click="counterStore.increment">increment counter store</button>
 
 	<!-- simple couter --> 
- 	<h1>cout: {{count}}</h1>
-    <button type="button" @click="count++">count is {{ count }}</button>
+ 	<h1 class="mt-2">cout: {{count}}</h1>
+	<button class="button" @click="count++">count is {{ count }}</button>
   </div>
 </template>
 
 <style>
+
+button {
+	@apply bg-gray-500 hover:bg-gray-700 transition duration-200 rounded p-1;
+}
+
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		"./index.html",
+		"./src/**/*.{vue,js,ts,jsx,tsx}",
+		"./src/App.vue",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
