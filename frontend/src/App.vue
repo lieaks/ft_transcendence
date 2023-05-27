7b9a25e616647a44f7e848c7394a42c89e449370
Rukkyaa <lbrt.axel@gmail.com>
2023-05-27 19:05:29 +0200

    Restart from the beginning

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+	import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+	<nav>
+		<RouterLink to="/">Home</RouterLink>
+		<RouterLink to="/game">Game</RouterLink>
+	</nav>
+	<RouterView />
+</template>
