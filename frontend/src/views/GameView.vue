36cab7eecb8816e4af598b3a6f22aa4cbb75fcb6
Rukkyaa <lbrt.axel@gmail.com>
2023-05-28 00:40:31 +0200

    Add redBall + ball at the center of the board

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -9,8 +9,11 @@
 		<img src="../assets/basic_map/ScoreBarP1.png" alt="ScoreBar" class="transform -scale-x-100" />
 		<img src="../assets/basic_map/ScoreBarP2.png" alt="ScoreBar" />
 	</div>
-	<img class="w-2/3 mx-auto" src="../assets/basic_map/Board.png" alt="Board" />
-	<!-- <img src="../assets/basic_map/Ball.png" alt="Ball"> -->
+	<div class="w-2/3 mx-auto relative">
+		<img src="../assets/basic_map/Board.png" alt="Board" class="w-full" />
+		<img src="../assets/basic_map/redBall.png" alt="Ball" class="absolute inset-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+	</div>
+	
 	<!-- <img src="../assets/basic_map/Player1.png" alt="Player1" /> -->
 	<!-- <img src="../assets/basic_map/Player2.png" alt="Player2" /> -->
 </template>
@@ -20,3 +23,4 @@
 		border: 1px solid red;
 	} */
 </style>
+
