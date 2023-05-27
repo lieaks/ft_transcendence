c8a22a6143f89988ff6325b9bc385d4e58a90ae2
Rukkyaa <lbrt.axel@gmail.com>
2023-05-28 00:29:39 +0200

    Add scorebar at the top of the board

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -3,12 +3,17 @@
 </script>
 
 <template>
-	<h1>Game</h1>
-	<img src="../assets/basic_map/Board.png" alt="Board" />
-	<img src="../assets/basic_map/Ball.png" alt="Ball">
-	<img src="../assets/basic_map/Player1.png" alt="Player1" />
-	<img src="../assets/basic_map/Player2.png" alt="Player2" />
-	<img src="../assets/basic_map/ScoreBar.png" alt="ScoreBar" />
+	<!-- <h1>Game</h1> -->
+
+	<!-- Create a flexbox to put 2 scorebar, centered, in the top of the board -->
+	<div class="flex justify-center">
+		<img src="../assets/basic_map/ScoreBarP1.png" alt="ScoreBar" class="transform -scale-x-100" />
+		<img src="../assets/basic_map/ScoreBarP2.png" alt="ScoreBar" />
+	</div>
+	<img class="w-2/3 mx-auto" src="../assets/basic_map/Board.png" alt="Board" />
+	<!-- <img src="../assets/basic_map/Ball.png" alt="Ball"> -->
+	<!-- <img src="../assets/basic_map/Player1.png" alt="Player1" /> -->
+	<!-- <img src="../assets/basic_map/Player2.png" alt="Player2" /> -->
 </template>
   
 <style>
