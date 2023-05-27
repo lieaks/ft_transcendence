c7421dfc3eb8df7b37f80a2ed912c241c1de7209
Rukkyaa <lbrt.axel@gmail.com>
2023-05-27 20:42:32 +0200

    Rename paddles + add assets to the GameView

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -5,6 +5,10 @@
 <template>
 	<h1>Game</h1>
 	<img src="../assets/basic_map/Board.png" alt="Board" />
+	<img srs="../assets/basic_map/Ball.png" alt="Ball" />
+	<img src="../assets/basic_map/Player1.png" alt="Player1" />
+	<img src="../assets/basic_map/Player2.png" alt="Player2" />
+	<img src="../assets/basic_map/ScoreBar.png" alt="ScoreBar" />
 </template>
   
 <style>
