b038277a0df514aede326bb73c575dd3f939117d
Axel LAMBERT <lbrt.axel@gmail.com>
2023-05-31 11:02:13 +0200

    Add paddles

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -4,33 +4,68 @@ import { onMounted, ref } from 'vue'
 const pongCanvas = ref<HTMLCanvasElement | null>(null);
 
 onMounted(() => {
-  const canvas = pongCanvas.value;
-  if (!canvas) return;
+	const canvas = pongCanvas.value;
+	if (!canvas) return;
 
-  const context = canvas.getContext('2d');
-  if (!context) return;
+	const context = canvas.getContext('2d');
+	if (!context) return;
 
-  const ball = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
-    radius: 10,
-    color: 'white'
-  };
+	const ball = {
+		x: canvas.width / 2,
+		y: canvas.height / 2,
+		radius: 10,
+		color: 'white'
+	};
 
-  function drawBall() {
-    context!.beginPath();
-    context!.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-    context!.fillStyle = ball.color;
-    context!.fill();
-    context!.closePath();
-  }
+	const rightPaddle = {
+		x: canvas.width - 40,
+		y: canvas.height / 2 - 75,
+		width: 20,
+		height: 150,
+		color: 'white'
+	};
 
-  function render() {
-	context!.clearRect(0, 0, canvas!.width, canvas!.height);
-	drawBall();
-	requestAnimationFrame(render);
-  }
+	const leftPaddle = {
+		x: 20,
+		y: canvas.height / 2 - 75,
+		width: 20,
+		height: 150,
+		color: 'red'
+	};
 
+	function drawBall() {
+		context!.beginPath();
+		context!.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+		context!.fillStyle = ball.color;
+		context!.fill();
+		context!.closePath();
+	}
+
+	function drawRightPaddle() {
+		context!.beginPath();
+		context!.rect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
+		context!.fillStyle = rightPaddle.color;
+		context!.fill();
+		context!.closePath();
+	}
+
+	function drawLeftPaddle() {
+		context!.beginPath();
+		context!.rect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
+		context!.fillStyle = leftPaddle.color;
+		context!.fill();
+		context!.closePath();
+	}
+
+	function render() {
+		context!.clearRect(0, 0, canvas!.width, canvas!.height);
+		drawBall();
+		drawRightPaddle();
+		drawLeftPaddle();
+		requestAnimationFrame(render);
+	}
+
+	render();
 });
 </script>
 
