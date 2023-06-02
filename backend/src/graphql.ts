f4df398fda9a8c35e1b66fc1f13339d58c38b9cb
mirsella <mirsella@protonmail.com>
2023-06-02 17:02:22 +0200

    update gpl.ts def

--- backend/src/graphql.ts
+++ backend/src/graphql.ts
@@ -19,6 +19,34 @@ export interface UpdateUserInput {
     friends?: Nullable<string[]>;
 }
 
+export interface Game {
+    __typename?: 'Game';
+    id: string;
+    players: User[];
+    winnerId?: Nullable<string>;
+    createdAt: string;
+    finishedAt?: Nullable<string>;
+}
+
+export interface IQuery {
+    __typename?: 'IQuery';
+    game(id: string): Nullable<Game> | Promise<Nullable<Game>>;
+    user(id: string): Nullable<User> | Promise<Nullable<User>>;
+    userName(name: string): Nullable<User> | Promise<Nullable<User>>;
+    users(id?: Nullable<string[]>): Nullable<Nullable<User>[]> | Promise<Nullable<Nullable<User>[]>>;
+    allUsers(): User[] | Promise<User[]>;
+}
+
+export interface IMutation {
+    __typename?: 'IMutation';
+    createGame(userId: string, enemyUserId: string): Game | Promise<Game>;
+    createUser(input: CreateUserInput): User | Promise<User>;
+    updateUser(id: string, input: UpdateUserInput): User | Promise<User>;
+    enable2FA(id: string): string | Promise<string>;
+    disable2FA(id: string): boolean | Promise<boolean>;
+    verify2FA(id: string, code: string): boolean | Promise<boolean>;
+}
+
 export interface User {
     __typename?: 'User';
     id: string;
@@ -32,34 +60,6 @@ export interface User {
     gameHistory?: Nullable<Game[]>;
     friends?: Nullable<User[]>;
     friendOf?: Nullable<User[]>;
-}
-
-export interface Game {
-    __typename?: 'Game';
-    id: string;
-    players: User[];
-    winnerId?: Nullable<string>;
-    createdAt: string;
-    finishedAt?: Nullable<string>;
-}
-
-export interface IQuery {
-    __typename?: 'IQuery';
-    user(id: string): Nullable<User> | Promise<Nullable<User>>;
-    userName(name: string): Nullable<User> | Promise<Nullable<User>>;
-    users(id?: Nullable<string[]>): Nullable<Nullable<User>[]> | Promise<Nullable<Nullable<User>[]>>;
-    allUsers(): User[] | Promise<User[]>;
-    game(id: string): Nullable<Game> | Promise<Nullable<Game>>;
-}
-
-export interface IMutation {
-    __typename?: 'IMutation';
-    createUser(input: CreateUserInput): User | Promise<User>;
-    updateUser(id: string, input: UpdateUserInput): User | Promise<User>;
-    createGame(userId: string, enemyUserId: string): Game | Promise<Game>;
-    enable2FA(id: string): string | Promise<string>;
-    disable2FA(id: string): boolean | Promise<boolean>;
-    verify2FA(id: string, code: string): boolean | Promise<boolean>;
 }
 
 type Nullable<T> = T | null;
