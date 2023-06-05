6f060ce764df3c5db685f18827b728d97f459a5a
Rukkyaa <lbrt.axel@gmail.com>
2023-06-05 14:57:00 +0200

    Still WIP

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -1,5 +1,5 @@
 <template>
-	<canvas ref="pongCanvas" width="1600" height="800" class="mx-auto bg-black"></canvas>
+    <canvas ref="pongCanvas" width="1600" height="800" class="mx-auto bg-black"></canvas>
 </template>
 
 <script setup lang="ts">
@@ -8,84 +8,76 @@ import { Ball } from '../stores/Ball';
 import { Paddle } from '../stores/Paddle';
 import { Game } from '../stores/Game';
 import { Scoreboard } from '@/stores/Scoreboard';
-
-// socket.io
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
-
-socket.on('connect', () => {
-	console.log('connected');
-});
-
-socket.on('disconnect', () => {
-	console.log('disconnected');
-});
-
-socket.on('message', (message: string) => {
-	console.log(message);
-});
-
-socket.emit('message', 'Hello World!');
 const pongCanvas = ref<HTMLCanvasElement | null>(null);
+const socket = io('http://localhost:3000');
 
 onMounted(() => {
 
-	const canvas = pongCanvas.value;
-	if (!canvas) return;
+    const canvas = pongCanvas.value;
+    if (!canvas) return;
 
-	const ball = new Ball(canvas.width / 2, canvas.height / 2, 10, 'white');
+    const ball = new Ball(canvas.width / 2, canvas.height / 2, 10, 'white');
 
-	const paddle = {
-		width: 20,
-		height: 175,
-		speed: 17
-	}
+    const paddle = {
+        width: 20,
+        height: 175,
+        speed: 17
+    }
 
-	const leftPaddle = new Paddle(canvas.width - 20 - paddle.width,
-		canvas.height / 2 - paddle.height / 2,
-		paddle.width, paddle.height,
-		paddle.speed,
-		'blue'
-	);
+    const leftPaddle = new Paddle(canvas.width - 20 - paddle.width,
+        canvas.height / 2 - paddle.height / 2,
+        paddle.width, paddle.height,
+        paddle.speed,
+        'blue'
+    );
 
-	const rightPaddle = new Paddle(20,
-		canvas.height / 2 - paddle.height / 2,
-		paddle.width, paddle.height,
-		paddle.speed,
-		'red'
-	);
+    const rightPaddle = new Paddle(20,
+        canvas.height / 2 - paddle.height / 2,
+        paddle.width, paddle.height,
+        paddle.speed,
+        'red'
+    );
 
-	const leftScore = new Scoreboard(0, canvas.width / 2 - 100, 75, 'white', "40px Arial");
-	const rightScore = new Scoreboard(0, canvas.width / 2 + 100 - 20, 75, 'white', "40px Arial");
+    const leftScore = new Scoreboard(0, canvas.width / 2 - 100, 75, 'white', "40px Arial");
+    const rightScore = new Scoreboard(0, canvas.width / 2 + 100 - 20, 75, 'white', "40px Arial");
 
-	const game = new Game(canvas, ball, leftPaddle, rightPaddle, leftScore, rightScore);
+    const game = new Game(canvas, ball, leftPaddle, rightPaddle, leftScore, rightScore);
 
     window.addEventListener('keydown', (event) => {
-		if (event.key === 'w' || event.key === 's') {
-			game.updateLeftPaddlePosition(event.key);
-		}
+        if (event.key === 'w' || event.key === 's') {
+            game.updateLeftPaddlePosition(event.key);
+        }
         if (event.key === 'i' || event.key === 'k') {
             game.updateRightPaddlePosition(event.key);
         }
     });
 
-	function gameLoop() {
-		game.render();
-		requestAnimationFrame(gameLoop);
-	}
-	gameLoop();
+    function gameLoop() {
+        game.render();
+        requestAnimationFrame(gameLoop);
+    }
+    gameLoop();
+
+    // function sendMessage() {
+    //     socket.emit('hello', 'Hello from client');
+    // }
+
+    // socket.on('onHello', (data) => {
+    //     console.log(data);
+    // });
 });
 </script>
 
 <style scoped>
-	/* Debug border */
-	/* * {
-		border: 1px solid red;
-	} */
-	canvas {
-		width: 100vw;
-		height: auto;
-		max-width: 1600px;
-	}
+    /* Debug border */
+    /* * {
+        border: 1px solid red;
+    } */
+    canvas {
+        width: 100vw;
+        height: auto;
+        max-width: 1600px;
+    }
 </style>
