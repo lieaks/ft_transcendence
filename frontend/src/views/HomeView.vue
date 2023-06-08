be23ef78d931f6b6a1f74b161c8d393bf677ade3
Rukkyaa <lbrt.axel@gmail.com>
2023-06-08 16:15:04 +0200

    Clean the HomeView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,14 +1,13 @@
 <template>
 	<div>
-	  <h1 class="text-white text-1xl">HOME VIEW</h1>
-	  <button @click="fetchChirelData">Fetch Data</button>
-	  <p>Chirel data: {{ getChirelData() }}</p>
+	<h1 class="text-white text-1xl">HOME VIEW</h1>
+	<button @click="fetchChirelData">Fetch Data</button>
+	<p>Chirel data: {{ getChirelData() }}</p>
 	</div>
-  </template>
+</template>
   
-  <script setup lang="ts">
+<script setup lang="ts">
 	import { useTestStore } from '@/stores/testStore';
-	import { ref } from 'vue';
-  
+
 	const { fetchChirelData, getChirelData } = useTestStore();
-  </script>
+</script>
