ad917c66d6ac6512e7990ac173562f6e0e3c2417
mirsella <mirsella@protonmail.com>
2023-06-10 21:43:23 +0200

    frontend: remove graphqlbyte type from userstore for now

--- frontend/src/stores/userStore.ts
+++ frontend/src/stores/userStore.ts
@@ -1,19 +1,19 @@
 import { GraphQLByte } from 'graphql-scalars'
 import { useQuery } from '@vue/apollo-composable'
 import { defineStore } from 'pinia'
-import { ref, computed } from 'vue'
+import { ref, computed, type Ref } from 'vue'
 
 export const useUserStore = defineStore('user', () => {
 	const id = '';
 	const jwtToken = ''
 	const name = ref('name')
-	const avatar = ref<GraphQLByte>('')
+	const avatar = ref('')
 
 	async function setName(newName: string) {
 		// gpl mutate back
 		name.value = newName
   }
-	async function setAvatar(newAvatar: typeof GraphQLByte) { // blob, buffer ?
+	async function setAvatar(newAvatar: string) { // blob, buffer ?
 		// gpl mutate back
 		avatar.value = newAvatar
 	}
