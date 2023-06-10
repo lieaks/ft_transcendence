37da5a867d8c578d18397f4db23a1cff59c5fae4
Rukkyaa <lbrt.axel@gmail.com>
2023-06-10 13:57:14 +0200

    Add player and removePlayer function

--- backend/src/games/game.ts
+++ backend/src/games/game.ts
@@ -3,20 +3,30 @@ import { PrismaService } from "../prisma/prisma.service";
 import { User } from "../users/user";
 
 export class Game {
-  id: string;
-  status: gameStatus;
-  players: User[];
-  winner: User;
-  createdAt: Date;
+	id: string;
+	status: gameStatus;
+	players: User[];
+	winner: User;
+	createdAt: Date;
 
-  constructor(
-	private readonly prismaService: PrismaService,
-	id: string,
-  ) {
-	this.id = id;
-	this.status = gameStatus.WAITING;
-	this.players = [];
-	this.winner = null;
-	this.createdAt = new Date();
-  }
+	constructor(
+		private readonly prismaService: PrismaService,
+		id: string,
+	) {
+		this.id = id;
+		this.status = gameStatus.WAITING;
+		this.players = [];
+		this.winner = null;
+		this.createdAt = new Date();
+	}
+
+	addPlayer(player: User): void {
+		this.players.push(player);
+	}
+
+	removePlayer(player: User): void {
+		this.players = this.players.filter((p) => p.id !== player.id);
+	}
+
+	// Function to create the game in the database
 }
