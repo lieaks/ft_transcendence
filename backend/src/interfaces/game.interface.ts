a11458ba56b48daf0aaea1ffb99c52636582608c
Rukkyaa <lbrt.axel@gmail.com>
2023-06-10 23:34:44 +0200

    add Interface for Game

--- backend/src/interfaces/game.interface.ts
+++ backend/src/interfaces/game.interface.ts
@@ -1,5 +1,19 @@
+import { User } from '../users/user';
+
 export enum gameStatus {
 	WAITING,
 	PLAYING,
 	ENDED
+}
+
+export interface IGame {
+	id: string;
+	status: gameStatus;
+	players: User[];
+	createdAt: Date;
+
+	addPlayer(player: User): void;
+	removePlayer(player: User): void;
+	create(): Promise<void>;
+	finish(winner: User, loser: User): Promise<void>;
 }
