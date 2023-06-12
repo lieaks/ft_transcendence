de536737c7ab764101ee00ac7837efa3513d43ef
mirsella <mirsella@protonmail.com>
2023-06-12 12:27:10 +0200

    frontend: fix compilation

--- frontend/src/views/AuthCallbackView.vue
+++ frontend/src/views/AuthCallbackView.vue
@@ -4,12 +4,24 @@ import { useUserStore } from '@/stores/userStore';
 
 const route = useRoute();
 const user = useUserStore()
-user.id = route.query.id
-user.jwtToken = route.query.id
+if (Array.isArray(route.query.id)) {
+	user.id = route.query.id[0]?.toString() || '';
+} else {
+	user.id = route.query.id || '';
+}
+if (Array.isArray(route.query.jwtToken)) {
+	user.jwtToken = route.query.jwtToken[0]?.toString() || '';
+} else {
+	user.jwtToken = route.query.jwtToken || '';
+}
 // request totp if needed
 // call a store function to get the avatar and name from the server
 // temporary :
-user.name = route.query.id
+if (Array.isArray(route.query.id)) {
+	user.name = route.query.id[0]?.toString() || '';
+} else {
+	user.name = route.query.id || '';
+}
 </script>
 
 <template>
