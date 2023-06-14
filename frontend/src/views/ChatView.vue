33142705886d00b2d344ff37b9d1ff8d906b77ed
davidlejaune <david_ly@hotmail.fr>
2023-06-14 12:24:22 +0200

    chat components added, still trying things. commit for school

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ChannelList from "@/components/chat/ChannelList.vue"
+</script>
 
 <template>
   <h1 class="text-white text-1xl">chat</h1>
+  <ChannelList />
 </template>
