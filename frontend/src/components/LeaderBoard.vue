8d0107861399deca2c700141d3473bd68f59e804
Axel LAMBERT <lbrt.axel@gmail.com>
2023-06-15 15:24:52 +0200

    feat: vlepille and aboulest with us

--- frontend/src/components/LeaderBoard.vue
+++ frontend/src/components/LeaderBoard.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
  
 import { ref } from 'vue'
+import alan from '@/assets/aboulest.jpg'
+import valentin from '@/assets/vlepille.jpg'
 
  const players = ref([
   { id: 1, name: 'John Doe', points: 100, avatar: 'https://i.pravatar.cc/150?img=1', win: 9, loose: 4 },
   { id: 2, name: 'Jane Doe', points: 90, avatar: 'https://i.pravatar.cc/150?img=2', win: 2, loose: 1 },
   { id: 3, name: 'Bob Smith', points: 80, avatar: 'https://i.pravatar.cc/150?img=3', win: 7, loose: 12 },
-  { id: 3, name: 'Aboulest', points: 60, avatar: 'https://i.pravatar.cc/150?img=4', win: 2, loose: 17 },
-  { id: 3, name: 'Vlepille', points: 40, avatar: 'https://i.pravatar.cc/150?img=5', win: 0, loose: 10 },
+  { id: 3, name: 'Aboulest', points: 60, avatar: alan, win: 2, loose: 17 },
+  { id: 3, name: 'Vlepille', points: 40, avatar: valentin, win: 0, loose: 10 },
   { id: 3, name: 'Giraffe', points: 20, avatar: 'https://i.pravatar.cc/150?img=6', win: 3, loose: 1 },
   { id: 3, name: 'Jquil', points: 5, avatar: 'https://i.pravatar.cc/150?img=7', win: 19, loose: 0 },
 ])
+
 
 </script>
 
