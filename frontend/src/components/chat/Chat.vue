6fc0fd9d2105778fa177a926848502ea93ffb92e
davidlejaune <david_ly@hotmail.fr>
2023-06-15 17:11:03 +0200

    new component: chat

---
+++ frontend/src/components/chat/Chat.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex-row p-4">
+    <h3 class="mb-3 mt-3">chanel list</h3>
+	<div class="overflow-scroll">
+
+	</div>
+    <div>
+		<input v-model="messageContent" class="form-control" @keydown.enter="sendMessage()" placeholder="Envoyer un message">
+		<button class="btn btn-primary" @click="sendMessage()">send</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useUserStore } from '@/stores/userStore'
+
+const user = useUserStore()
+const messageContent = ref("");
+
+function sendMessage() {
+	user.socket?.emit(messageContent.value);
+	messageContent.value = "";
+}
+</script>
+
+<style></style>
