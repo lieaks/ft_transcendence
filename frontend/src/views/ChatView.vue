dec1d0365ddc77c5b4e8ed96008b1858aa36459d
davidlejaune <david_ly@hotmail.fr>
2023-06-16 22:10:14 +0200

    Chatstore added

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import ChannelList from '@/components/chat/ChannelList.vue'
+import Chat from '@/components/chat/Chat.vue'
 </script>
 
 <template>
   <h1 class="text-white text-1xl">chat</h1>
-  <ChannelList />
+  <!-- <ChannelList /> -->
+  <Chat />
 </template>
