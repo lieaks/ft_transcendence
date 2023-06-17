7de0061ec284df58b5e6a5870c280019472ecd01
mirsella <mirsella@protonmail.com>
2023-06-17 23:26:16 +0200

    feat(frontend/views/HomeView.vue): clean up

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-import { useTestStore } from '@/stores/testStore'
 import { useUserStore } from '@/stores/userStore'
 import { useRouter } from 'vue-router'
 import MatchmakingComponent from '@/components/MatchmakingComponent.vue'
 import LeaderboardComponent from '@/components/LeaderBoardComponent.vue'
 
-// const { fetchChirelData, notLoveChirel, loveChirel, getChirelData } = useTestStore()
 const user = useUserStore()
 const router = useRouter()
-
-function redirectToOAuth(provider: string) {
-  window.location.href = `${import.meta.env.VITE_BACKEND_URL}/auth/${provider}`
-}
 
 function joinQueue() {
   user.socket?.emit('joinQueue', {})
@@ -26,50 +20,9 @@ user.socket?.on('startGame', (data) => {
 </script>
 
 <template>
-  <div>
-    <!-- <h1 class="text-white text-2xl">HOME VIEW</h1> -->
-    <!-- <button @click="fetchChirelData">Fetch Data</button> -->
-    <!-- <p>Chirel data: {{ getChirelData() }}</p> -->
-    <!-- <button @click="notLoveChirel">Not Love Chirel</button> -->
-    <!-- <button @click="loveChirel">Love Chirel</button> -->
-    <button class="btn btn-primary m-4" @click="joinQueue">Join Queue</button>
+  <div class="flex justify-center items-center">
+    <button v-if="!user.inQueue" class="btn btn-primary m-4" @click="joinQueue">Join Queue</button>
     <MatchmakingComponent v-if="user.inQueue" />
-  </div>
-
-  <div class="p-4">
-    <h1>dev buttons</h1>
-    <input
-      type="text"
-      placeholder="user's name"
-      v-model="user.name"
-      class="input input-bordered m-4"
-    />
-    <button class="btn btn-primary m-2" onclick="localStorage.removeItem('jwtToken')">
-      delete jwtToken
-    </button>
-    <button class="btn btn-primary m-2" @click="redirectToOAuth('42')">login 42</button>
-    <button class="btn btn-primary m-2" @click="redirectToOAuth('google')">login google</button>
   </div>
   <LeaderboardComponent />
 </template>
-
-<style scoped>
-/* button { */
-/*   background-color: #4caf50; */
-/*   color: #fff; */
-/*   padding: 0.5rem 1rem; */
-/*   border: none; */
-/*   border-radius: 0.25rem; */
-/*   cursor: pointer; */
-/*   margin-right: 1rem; */
-/* } */
-
-/* button:hover { */
-/*   background-color: #3e8e41; */
-/* } */
-
-p {
-  margin-top: 1rem;
-  font-size: 1.25rem;
-}
-</style>
