550c57744c1028aad018ceea94300158ce853277
mirsella <mirsella@protonmail.com>
2023-06-22 00:01:17 +0200

    style(front): ran npm run format

--- frontend/src/components/chat/ChatComponent.vue
+++ frontend/src/components/chat/ChatComponent.vue
@@ -1,37 +1,39 @@
 <script setup lang="ts">
 import type { IChannel, IUser, IMessage } from '@/interfaces/chat.interfaces'
-import { useUserStore } from '@/stores/userStore';
-import { ref, type PropType } from 'vue';
+import { useUserStore } from '@/stores/userStore'
+import { ref, type PropType } from 'vue'
 
 const user = useUserStore()
 const newMessage = ref('')
 
 const props = defineProps({
-	channel: {
-		type: Object as PropType<IChannel>,
-		required: true
-	},
-});
+  channel: {
+    type: Object as PropType<IChannel>,
+    required: true
+  }
+})
 
 function sendMessage() {
-	if (!newMessage.value) return
-	user?.socket.emit('sendMessage', { channelId: props.channel.id, content: newMessage.value })
-	newMessage.value = ''
+  if (!newMessage.value) return
+  user?.socket.emit('sendMessage', { channelId: props.channel.id, content: newMessage.value })
+  newMessage.value = ''
 }
-
 </script>
 
 <template>
-	<div>
-		channel {{ channel.name }}
-		<ul>
-			<li v-for="message in channel.messages">
-				{{ message.sender.name }}: {{ message.content }}
-			</li>
-		</ul>
-		<div class="inline">
-			<input type="text" class="input input-secondary m-2" placeholder="new message" v-model="newMessage">
-			<button class="btn btn-secondary m-2" @click="sendMessage()">send</button>
-		</div>
-	</div>
+  <div>
+    channel {{ channel.name }}
+    <ul>
+      <li v-for="message in channel.messages">{{ message.sender.name }}: {{ message.content }}</li>
+    </ul>
+    <div class="inline">
+      <input
+        type="text"
+        class="input input-secondary m-2"
+        placeholder="new message"
+        v-model="newMessage"
+      />
+      <button class="btn btn-secondary m-2" @click="sendMessage()">send</button>
+    </div>
+  </div>
 </template>
--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -2,8 +2,8 @@
 import ChatComponent from '@/components/chat/ChatComponent.vue'
 import ChannelList from '@/components/chat/ChannelList.vue'
 import type { IChannel, IUser, IMessage } from '@/interfaces/chat.interfaces'
-import { useUserStore } from '@/stores/userStore';
-import { ref, type Ref } from 'vue';
+import { useUserStore } from '@/stores/userStore'
+import { ref, type Ref } from 'vue'
 
 const user = useUserStore()
 
@@ -13,57 +13,56 @@ let activeChannel: Ref<IChannel | undefined> = ref(undefined)
 user.socket.emit('channelAvailable')
 
 user?.socket.on('channelAvailable', (newChannels: IChannel[]) => {
-	console.log('channelAvailable', newChannels)
-	availableChannels.value = newChannels
+  console.log('channelAvailable', newChannels)
+  availableChannels.value = newChannels
 })
 user?.socket.on('newChannel', (newChannel: IChannel) => {
-	console.log('newChannel', newChannel)
-	if (!newChannel) return
-	availableChannels.value.push(newChannel)
+  console.log('newChannel', newChannel)
+  if (!newChannel) return
+  availableChannels.value.push(newChannel)
 })
 
-user?.socket.on('userJoined', (newUser: {channelId: string, user: IUser }) => {
-	console.log('userJoined', newUser)
-	if (!newUser) return
-	let channel = joinedChannels.value.find(channel => channel.id === newUser.channelId)
-	if (channel) {
-		channel.users.push(newUser.user)
-	} else {
-		availableChannels.value = availableChannels.value.filter(c => {
-			if (c.id === newUser.channelId) {
-				if (!c.users) c.users = []
-				c?.users.push(newUser.user)
-				joinedChannels.value.push(c)
-				return false
-			} else {
-				return true
-			}
-		})
-	}
+user?.socket.on('userJoined', (newUser: { channelId: string; user: IUser }) => {
+  console.log('userJoined', newUser)
+  if (!newUser) return
+  let channel = joinedChannels.value.find((channel) => channel.id === newUser.channelId)
+  if (channel) {
+    channel.users.push(newUser.user)
+  } else {
+    availableChannels.value = availableChannels.value.filter((c) => {
+      if (c.id === newUser.channelId) {
+        if (!c.users) c.users = []
+        c?.users.push(newUser.user)
+        joinedChannels.value.push(c)
+        return false
+      } else {
+        return true
+      }
+    })
+  }
 })
 
-user?.socket.on('newMessage', (newMessage: { channelId: string, message: IMessage }) => {
-	console.log('newMessage', newMessage)
-	if (!newMessage?.channelId || !newMessage?.message) return
-	let channel = joinedChannels.value.find(channel => channel.id === newMessage.channelId)
-	if (channel) {
-		if (!channel.messages) channel.messages = []
-		channel.messages.push(newMessage.message)
-	}
+user?.socket.on('newMessage', (newMessage: { channelId: string; message: IMessage }) => {
+  console.log('newMessage', newMessage)
+  if (!newMessage?.channelId || !newMessage?.message) return
+  let channel = joinedChannels.value.find((channel) => channel.id === newMessage.channelId)
+  if (channel) {
+    if (!channel.messages) channel.messages = []
+    channel.messages.push(newMessage.message)
+  }
 })
-
 </script>
 
 <template>
-	<ChannelList :availableChannels="availableChannels"/>
-	<div class="m-2">
-		<ul>
-			<li v-for="channel in joinedChannels" :key="channel.id" class="inline">
-				<button class="btn btn-secondary m-2 normal-case" @click="activeChannel = channel">
-					{{ channel.name }}
-				</button>
-			</li>
-		</ul>
-	</div>
-	<ChatComponent v-if="joinedChannels.length" :channel="activeChannel || joinedChannels[0]"/>
+  <ChannelList :availableChannels="availableChannels" />
+  <div class="m-2">
+    <ul>
+      <li v-for="channel in joinedChannels" :key="channel.id" class="inline">
+        <button class="btn btn-secondary m-2 normal-case" @click="activeChannel = channel">
+          {{ channel.name }}
+        </button>
+      </li>
+    </ul>
+  </div>
+  <ChatComponent v-if="joinedChannels.length" :channel="activeChannel || joinedChannels[0]" />
 </template>
