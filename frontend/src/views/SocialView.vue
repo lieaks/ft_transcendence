6397379ccfd02e0cfa5a6fbf649b89a11df5c069
Rukkyaa <lbrt.axel@gmail.com>
2023-06-24 00:42:26 +0200

    clean()

--- frontend/src/views/SocialView.vue
+++ frontend/src/views/SocialView.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-import EndGameComponent from '@/components/EndGameComponent.vue';
 </script>
 
 <template>
   <h1 class="text-white text-1xl">fuzzy finder for profile, open profile component</h1>
   <h1 class="text-white text-1xl">same for blocked, blockedOf, friendsOf etc</h1>
-  <EndGameComponent card-type="lose"/>
-  <EndGameComponent card-type="win"/>
 </template>
