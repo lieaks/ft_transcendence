f9d4a04834a3869bbf0b60173ad54ed42443dfb1
mirsella <mirsella@protonmail.com>
2023-06-23 19:49:57 +0200

    feat(frontend/loginComponent): bigger button

--- frontend/src/components/LoginComponent.vue
+++ frontend/src/components/LoginComponent.vue
@@ -8,8 +8,8 @@ function redirectToOAuth(provider: string) {
 </script>
 
 <template>
-  <div>
-    <button class="btn btn-primary m-2" @click="redirectToOAuth('42')">login 42</button>
-    <button class="btn btn-primary m-2" @click="redirectToOAuth('google')">login google</button>
-  </div>
+	<div class="flex justify-center items-center h-full">
+		<button class="btn btn-primary m-4 md:m-12 h-full w-1/4 text-2xl" @click="redirectToOAuth('42')">login 42</button>
+		<button class="btn btn-primary m-4 md:m-12 h-full w-1/4 text-2xl" @click="redirectToOAuth('google')">login google</button>
+	</div>
 </template>
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -3,5 +3,9 @@ import LoginComponent from '@/components/LoginComponent.vue'
 </script>
 
 <template>
-  <LoginComponent />
+	<div class="h-screen mt-20">
+		<div class="h-1/3">
+			<LoginComponent />
+		</div>
+	</div>
 </template>
