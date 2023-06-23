bbeb47665fdf80b50b672f0ff8bd2b4cf248ffc4
Rukkyaa <lbrt.axel@gmail.com>
2023-06-23 23:34:56 +0200

    feat(game) end game component done

---
+++ frontend/src/components/EndGameComponent.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import router from '@/router';
+import winImg from '@/assets/win.jpg';
+import loseImg from '@/assets/lose.jpg';
+
+const props = defineProps({
+  cardType: {
+    type: String,
+    required: true,
+    validator: (value: string) => ['win', 'lose'].includes(value),
+  },
+})
+
+const cardImage = props.cardType === 'win' ? winImg : loseImg
+const cardAlt = props.cardType === 'win' ? 'win' : 'lose'
+const cardTitle = props.cardType === 'win' ? 'You Win!' : 'You Lose!'
+
+function backHome() {
+  router.push(`/`)
+}
+</script>
+
+<template>
+  <div class="card w-96 bg-base-100 shadow-xl">
+    <figure>
+      <img :src="cardImage" :alt="cardAlt" />
+    </figure>
+    <div class="card-body items-center text-center">
+      <h2 class="card-title">{{ cardTitle }}</h2>
+      <button class="btn btn-primary" @click="backHome">Back Home</button>
+    </div>
+  </div>
+</template>
--- frontend/src/views/SocialView.vue
+++ frontend/src/views/SocialView.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EndGameComponent from '@/components/EndGameComponent.vue';
+</script>
 
 <template>
   <h1 class="text-white text-1xl">fuzzy finder for profile, open profile component</h1>
   <h1 class="text-white text-1xl">same for blocked, blockedOf, friendsOf etc</h1>
+  <EndGameComponent card-type="lose"/>
 </template>
