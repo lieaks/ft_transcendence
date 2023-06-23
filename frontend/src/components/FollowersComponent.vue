cc0ccc5404216548b5da6896ef1332f75a4cf64f
Axel LAMBERT <lbrt.axel@gmail.com>
2023-06-23 16:37:21 +0200

    clean(followers) remove useless import

--- frontend/src/components/FollowersComponent.vue
+++ frontend/src/components/FollowersComponent.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { useUserStore } from '@/stores/userStore'
 import router from '@/router'
 import { useQuery } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
