b9f6d119c0c739baa7ed12c05b24c962fc631260
Rukkyaa <lbrt.axel@gmail.com>
2023-06-24 12:32:37 +0200

    feat(notify) some tests

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,7 @@ import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client/core'
 import { setContext } from '@apollo/client/link/context'
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify'
 
 import './assets/tailwind.css'
 
@@ -37,4 +38,8 @@ const app = createApp({
 
 app.use(router)
 app.use(createPinia().use(piniaPluginPersistedState))
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+  position: 'top-left',
+} as ToastContainerOptions)
 app.mount('#app')
--- frontend/src/views/SocialView.vue
+++ frontend/src/views/SocialView.vue
@@ -1,6 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { toast } from 'vue3-toastify';
+  import 'vue3-toastify/dist/index.css';
+
+  const notify = () => {
+    toast('Axel t\'as invite a rejoindre sa partie', {
+    });
+  };
+</script>
 
 <template>
   <h1 class="text-white text-1xl">fuzzy finder for profile, open profile component</h1>
   <h1 class="text-white text-1xl">same for blocked, blockedOf, friendsOf etc</h1>
+  <div>
+    <button @click="notify">Notify !</button>
+  </div>
 </template>
