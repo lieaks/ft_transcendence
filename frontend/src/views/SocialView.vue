dd27645e2790e36c850bdfd902bb4f221b7cfdc7
Rukkyaa <lbrt.axel@gmail.com>
2023-06-25 20:59:46 +0200

    fix(workflow)

--- frontend/src/views/SocialView.vue
+++ frontend/src/views/SocialView.vue
@@ -8,8 +8,8 @@
   <h1 class="text-white text-1xl">fuzzy finder for profile, open profile component</h1>
   <h1 class="text-white text-1xl">same for blocked, blockedOf, friendsOf etc</h1>
   <div>
-    <button @click="notifs.notifyGameInvite('Axel invite you in a game !')">Invite !</button>
-    <button @click="notifs.notifyError('Error')">Error !</button>
-    <button @click="notifs.notifySuccess('Valid')">Valid !</button>
+    <!-- <button @click="notifs.notifyGameInvite('Axel invite you in a game !')">Invite !</button> -->
+    <!-- <button @click="notifs.notifyError('Error')">Error !</button> -->
+    <!-- <button @click="notifs.notifySuccess('Valid')">Valid !</button> -->
   </div>
 </template>
